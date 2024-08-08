040d2530ef548b02093387c4da8d3e8a3aa92b4f
carector <baseline7@live.com>
2024-08-07 21:51:01 -0700

    Move firebase setup to standalone file

---
+++ src/backend/fb.js
@@ -0,0 +1,35 @@
+// Imports
+const { initializeApp } = require('firebase/app');
+const { initializeFirestore, collection, getDocs } = require('firebase/firestore');
+const { getAnalytics } = require('firebase/analytics');
+require('dotenv').config(); // TODO: Figure out why this is required for anything using .env file
+
+
+// Firebase setup
+let db;
+let app;
+
+const setupFirebase = async() => {
+	// Set up config
+	const firebaseConfig = {
+		apiKey: process.env.FIREBASE_API_KEY,
+		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+		appId: process.env.FIREBASE_APP_ID,
+		measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+	};
+
+	// Connect to app + db + analytics
+	app = initializeApp(firebaseConfig);
+	db = initializeFirestore(app, {
+        experimentalForceLongPolling: true,
+    });
+
+	console.log('Firebase initialized');
+	//fb_analytics = getAnalytics(fb_app);
+};
+
+setupFirebase();
+module.exports = { db };
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,56 +1,32 @@
 // Imports
-const { initializeApp } = require('firebase/app');
-const { getFirestore } = require('firebase/firestore');
-const { getAnalytics } = require('firebase/analytics');
 const express = require('express');
 const { Sequelize } = require('sequelize');
 const config = require('./config.json');
 const routes = require('./routes/index.route.js');
-require('dotenv').config()
+const { db } = require('./fb.js');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 // Definitions
 const port = config.port;
-let fb_app;
-let fb_analytics;
-let fb_db;
 let app;
 
 async function init() {
-	// Set up DB connection
-	try {
-		await setUpFirebase();
-	} catch (err) {
+	// Verify DB connection
+	if (!db) {
 		console.error(`Error initializing Firebase: ${err}`);
 		return;
 	}
 
-	// Set up app + routes
+	// Set up app
 	app = express();
+	app.use(bodyParser.json());
+	app.use(express.json());
 	app.use(routes);
 
 	app.listen(port, () => {
-		console.log(`Listening on port ${port}`);
+		console.log(`Done! Listening on port ${port}`);
 	});
-}
-
-async function setUpFirebase() {
-	// Set up config
-	const firebaseConfig = {
-		apiKey: process.env.FIREBASE_API_KEY,
-		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-		projectId: process.env.FIREBASE_PROJECT_ID,
-		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-		appId: process.env.FIREBASE_APP_ID,
-		measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-	};
-
-	// Connect to app + db + analytics
-	fb_app = initializeApp(firebaseConfig);
-	fb_db = getFirestore(fb_app);
-	//fb_analytics = getAnalytics(fb_app);
-
-	// Verify connection
 }
 
 // Initialize app
