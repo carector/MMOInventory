9742fa9f275b0af1f02d6635510309a34d5faa36
Caleb <baseline7@live.com>
2024-11-01 15:55:35 -0700

    Fix auth indentation

--- src/backend/controllers/auth.controller.js
+++ src/backend/controllers/auth.controller.js
@@ -5,44 +5,60 @@ const { toBool } = require('../common.js');
 // #endregion
 
 // #region Middleware functions
-const getCurrentUser = async function(req, res, next) { res.status(400).send("TODO"); }
-const createUserWithEmailAndPassword = async function(req, res, next) { 
-    const auth = getAuth();
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-        const result = await fbAuth.createUserWithEmailAndPassword(auth, email, password);
-        res.locals.authResult = result;
-        next();
-    }
-    catch(e) {
-        console.log(e);
+const getCurrentUser = async function (req, res, next) {
+	res.status(400).send('TODO');
+};
+
+const createUserWithEmailAndPassword = async function (req, res, next) {
+	const auth = getAuth();
+	try {
+		const email = req.body.email;
+		const password = req.body.password;
+		const result = await fbAuth.createUserWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
+		res.locals.authResult = result;
+		next();
+	} catch (e) {
+		console.log(e);
 		return res.status(400).send(`Error: ${e}`);
-    }
+	}
+};
 
- }
-
-const deleteUser = async function(req, res, next) { res.status(400).send("TODO"); }
-const resetPassword = async function(req, res, next) { res.status(400).send("TODO"); }
-const verifyPasswordResetCode = async function(req, res, next) { res.status(400).send("TODO"); }
-const setPersistence = async function(req, res, next) { res.status(400).send("TODO"); } // i.e. stay logged in checkbox
+const deleteUser = async function (req, res, next) {
+	res.status(400).send('TODO');
+};
+const resetPassword = async function (req, res, next) {
+	res.status(400).send('TODO');
+};
+const verifyPasswordResetCode = async function (req, res, next) {
+	res.status(400).send('TODO');
+};
+const setPersistence = async function (req, res, next) {
+	res.status(400).send('TODO');
+}; // i.e. stay logged in checkbox
 
 // Credentials
-const getGoogleCredential = async function(req, res, next){};
-const signInUsingCredential = async function(req, res, next){};
+const getGoogleCredential = async function (req, res, next) {};
+const signInUsingCredential = async function (req, res, next) {};
 
-const signIn = async function(req, res, next) { res.status(400).send("TODO"); }
-const signOut = async function(req, res, next) { res.status(400).send("TODO"); }
-
+const signIn = async function (req, res, next) {
+	res.status(400).send('TODO');
+};
+const signOut = async function (req, res, next) {
+	res.status(400).send('TODO');
+};
 
 // #endregion
 
 module.exports = {
 	getCurrentUser,
-    createUserWithEmailAndPassword,
-    resetPassword,
-    verifyPasswordResetCode,
-    setPersistence,
-    signIn,
-    signOut
+	createUserWithEmailAndPassword,
+	resetPassword,
+	verifyPasswordResetCode,
+	setPersistence,
+	signIn,
+	signOut,
 };
